8aab33110fa02456dca25b04cf6dd0c837919902
Oleh <olh.masiuk@gmail.com>
2021-11-23 15:57:33 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+    // Initialze winsock
+    WSADATA wsData;
+    WORD ver = MAKEWORD(2, 2);
+
+    int wsOk = WSAStartup(ver, &wsData);
+    if (wsOk != 0)
+    {
+        cerr << "Can't Initialize winsock! Quitting" << endl;
+        return;
+    }
+
+    // Create a socket
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == INVALID_SOCKET)
+    {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return;
+    }
+
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000);
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+    // Tell Winsock the socket is for listening 
+    listen(listening, SOMAXCONN);
+
+    // Wait for a connection
+    sockaddr_in client;
+    int clientSize = sizeof(client);
+
+    SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+
+    char host[NI_MAXHOST];        // Client's remote name
+    char service[NI_MAXSERV];    // Service (i.e. port) the client is connect on
+
+    ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+    ZeroMemory(service, NI_MAXSERV);
+
+    if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+    {
+        cout << host << " connected on port " << service << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on port " <<
+            ntohs(client.sin_port) << endl;
+    }
+
+    // Close listening socket
+    closesocket(listening);
+
+    // While loop: accept and echo message back to client
+    char buf[4096];
+    string SubsciptionAccepted = "Sucessfully subscripted to mailing service: client ";
+    SubsciptionAccepted += host;
+    SubsciptionAccepted += "..\n";
+
+    ZeroMemory(buf, 4096);
+
+    int bytesReceived = recv(clientSocket, buf, 4096, 0);
+    if (bytesReceived == SOCKET_ERROR)
+    {
+        cerr << "Error in recv(). Quitting" << endl;
+        exit(3);
+    }
+    int oper = atoi(buf);
+    cout << to_string(oper) + " option of mailing was chosed by client " + host;
+
+    send(clientSocket, SubsciptionAccepted.c_str(), SubsciptionAccepted.size() + 1, 0);
+
+
+    switch (oper) {
+    case 1:
+        do {
+            int i = 0;
+            send(clientSocket, buf, bytesReceived + 1, 0);
+            i++;
+        } while (true);
+    case 2:
+        do {
+            send(clientSocket, buf, bytesReceived + 1, 0);
+        } while (true);
+    case 3:
+        do {
+            send(clientSocket, buf, bytesReceived + 1, 0);
+        } while (true);
+    }
+
+    // Close the socket
+    closesocket(clientSocket);
+
+    // Cleanup winsock
+    WSACleanup();
+}
